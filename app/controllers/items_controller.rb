c8eb137bc7152e47356eb44b5ec9ec9ef574dc84
Rick0317 <201507037f@edu.nishiyamato.ed.jp>
2022-01-02 17:03:19 -0500

    display items commit 3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,17 +3,17 @@ class ItemsController < ApplicationController
   end
   def new
     if user_signed_in?
-      render new_item_path  
+      @item = Item.new
     else
       redirect_to new_user_session_path
-      @item = Itme.new
     end
   end
   def create
-    @item = Itme.new(item_params)
+    @item = Item.create(item_params)
     if @item.save
       redirect_to root_path
     else
+      @item = Item.new(item_params)
       render :new
     end
   end
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :category_id, :region_id, :ship_length_id, :shipping_cost_id, :state_id, :imgae)
+    params.require(:item).permit(:name, :price, :description, :category_id, :region_id, :ship_length_id, :shipping_cost_id, :state_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,16 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :state, :shipping_cost, :region, :ship_length
 
-  validates :category_id, :state_id, :shipping_cost_id, :region_id, :ship_length_id, numericality: { other_than: 1 , message: "can't be blank"} 
   
-
-  validates :name, :price, presence: true
+  
+  with_options presence: true, numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id, :state_id, :shipping_cost_id, :region_id, :ship_length_id
+  end
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } do
+    validates :price
+  end
+  validates :name, presence: true
   validates :description, presence: true
-  validates :state_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :ship_length_id, presence: true
-  validates :region_id, presence:true
-  validates :category_id, presence:true
 
   has_one_attached :image
 end
