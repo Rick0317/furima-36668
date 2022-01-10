07f0a045694a8b6614c28af0c992ce675e16e41e
Rick0317 <201507037f@edu.nishiyamato.ed.jp>
2022-01-10 09:13:10 -0500

    display items commit 6

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -74,7 +74,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
       it 'userが紐づいていないと出品できない' do
-        
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
