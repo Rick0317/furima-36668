6a316a91e07a6bac33aabf40b0291b92146b762e
Rick0317 <201507037f@edu.nishiyamato.ed.jp>
2022-01-14 14:15:34 -0500

    item editing commit 2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,21 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      @item = Item.new(item_params)
+      render :edit
+    end
+  end
 
   private
 
